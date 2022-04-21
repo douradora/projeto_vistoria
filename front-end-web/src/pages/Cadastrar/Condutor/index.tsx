abf1194f3bbc4bc25e5d7f9f64236198a5592aaa
douradora <ramonsgc@gmail.com>
2022-04-20 21:14:46 -0300

    alteraçoes feitas em meu computador

--- front-end-web/src/pages/Cadastrar/Condutor/index.tsx
+++ front-end-web/src/pages/Cadastrar/Condutor/index.tsx
@@ -1,12 +1,12 @@
 import { Scope } from '@unform/core';
 import React from 'react';
 import Input from '../../../componetes/Input';
-
+import './styles.css';
 
 
 const Condutor: React.FC = () => {
   return (
-        <div className='condutor-form'>
+        <div className='conteiner-condutor'>
           <Scope path='Condutor'>
          <Input name='Nome_condutor' label='Nome Completo:'/>
          <Input name='cpf_condutor' label='Cpf:'/>
