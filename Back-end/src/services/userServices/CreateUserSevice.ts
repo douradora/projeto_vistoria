592f37de254148d1ad0421bb08f5b100b64446ab
douradora <ramonsgc@gmail.com>
2022-09-09 08:13:08 -0300

    refatoraçao dos nomes

--- Back-end/src/services/userServices/CreateUserSevice.ts
+++ Back-end/src/services/userServices/CreateUserSevice.ts
@@ -8,11 +8,13 @@ export class createUserService {
  * @param adm nao é obrigatorio 
  * @returns retorna o usuario salvo  caso sucesso
  */
-    async execute({ cpf_user, name, email, password,is_adm}: IUSerRequest) {
+    async execute(User: IUSerRequest) {
 
-        const findCpfExists = await Client.user.findFirst({where:{cpf_user:cpf_user}});
+        const{ cpf_user, name, email, password,is_adm} =User;
 
-        const findemailExists = await Client.user.findFirst({where:{email:email}});
+        const findCpfExists = await Client.user.findFirst({where:{cpf_user}});
+
+        const findemailExists = await Client.user.findFirst({where:{email}});
         
         if (findCpfExists){
             throw new Error("cpf ja cadastrado");
@@ -20,13 +22,14 @@ export class createUserService {
         if (findemailExists){
             throw new Error("email esta em uso por outro usuario");
         }
-        const pW = await hash(password, 10);
+        const pWCripto = await hash(password, 10);
 
         const user = await Client.user.create( {
-            data:{cpf_user,
+            data:{
+            cpf_user,
             name,
             email,
-            password: pW,
+            password: pWCripto,
             is_adm,
             
             }
